efc48701b20abf555f38bbde4f7c2f887702e50c
Markéta Křenková <marketa@Marketa-MacBook-Air.local>
2018-10-15 20:55:09 +0200

    smazani strukturnich radku

--- nlp1/desam.py
+++ nlp1/desam.py
@@ -3,7 +3,6 @@ import sys
 for line in sys.stdin:
     columns = line.rstrip('\r\n').split("\t")
     if len(columns) < 4:
-        print(line, end = "")
         continue
     code = columns[2]
     new_codes = columns[3].split(":")[2::2]
